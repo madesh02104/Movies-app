0eff4f1d1586da3cd9fc2ce436e73efd70ab5f91
madesh02104 <madesh02104@gmail.com>
2025-03-26 11:32:39 +0530

    added db_url for production on railway

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
